ef4e6ccececa619490899c375d751bfb34170fee
Tomassss2005 <tomidlp2005@gmail.com>
2024-11-27 11:23:41 -0300

    Home terminado

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Inicio from "./components/inicio.jsx";
+import Carrusel from "./components/carrusel.jsx";
+import TresTratamientos from "./components/tresTratamientos.jsx";
+import Consejos from "./components/consejos.jsx";
 import Footer from "./components/footer.jsx";
 import Contacto from "./components/contacto.jsx";
+import Perfil from "./components/Perfil.jsx";
+
 
 function App() {
   return (
     <div>
       <Inicio />
       <Routes>
+        <Route
+          path='/'
+          element={
+            <>
+              <Carrusel />
+              <TresTratamientos />
+              <Consejos />
+            </>
+          }
+        />
         <Route path='/contacto' element={<Contacto />} />
+        <Route path='/mi-perfil' element={<Perfil />} />
       </Routes>
       <Footer />
     </div>
---
+++ src/components/tresTratamientos.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import styles from "../styles/tresTratamientosModule.css";
+
+
+function Tratamientos() {
+    return (
+        <div className={`tratamientos_big_container ${styles.tratamientos_big_container}`}>
+            <h1 className={`titulo_tratamientos ${styles.titulo_tratamientos}`}>Algunos de nuestros tratamientos</h1>
+            <div className={`tratamientos_container ${styles.tratamientos_container}`}>
+                <div className={`tratamiento ${styles.tratamiento}`}>
+                    <img className={`img_tratamientos ${styles.img_tratamientos}`} src="/primer-tratamiento.jpg" alt="Tratamiento 1" />
+                    <Link to="/plasma-rico-en-plaquetas" className={`a_tratamientos ${styles.a_tratamientos}`}>Plasma rico en plaquetas</Link>
+                </div>
+                <div className={`tratamiento ${styles.tratamiento}`}>
+                    <img className={`img_tratamientos ${styles.img_tratamientos}`} src="/segundo-tratamiento.jpg" alt="Tratamiento 2" />
+                    <Link to="/peeling-facial" className={`a_tratamientos ${styles.a_tratamientos}`}>Peeling facial</Link>
+                </div>
+                <div className={`tratamiento ${styles.tratamiento}`}>
+                    <img className={`img_tratamientos ${styles.img_tratamientos}`} src="/tercer-tratamiento.jpg" alt="Tratamiento 3" />
+                    <Link to="/toxina-botulinica" className={`a_tratamientos ${styles.a_tratamientos}`}>Toxina botulínica</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Tratamientos;
