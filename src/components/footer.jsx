5663e6bd28033a6f7934d9a81fa3c51b645f13fe
Tomassss2005 <tomidlp2005@gmail.com>
2024-11-30 16:33:33 -0300

    Algunos cambios mas

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -37,7 +37,7 @@ function Footer() {
                     <h3 className={`footer_h3 ${styles.footer_h3}`}>Contacto</h3>
                     <p> <a href="mailto:erikaromero.salud@gmail.com" className={`a_p_footer ${styles.a_p_footer}`}>EMAIL: erikaromero.salud@gmail.com</a></p>
                     <p> <a href="https://wa.me/+5491137742782" className={`a_p_footer ${styles.a_p_footer}`}>TELÉFONO: +54 9 11 3774-2782</a></p>
-                    <p> <a href="https://www.google.com/maps/place/Av.Triunvirato+3785,+C1431FDO+CABA" className={`a_p_footer ${styles.a_p_footer}`}>DIRECCIÓN: Av.Triunvirato 3785,1°A.</a></p>
+                    <p> <a href="https://www.google.com/maps/place/Av.Triunvirato+3785,+C1431FDO+CABA" className={`a_p_footer_3 ${styles.a_p_footer_3}`}>DIRECCIÓN: Av.Triunvirato 3785,1°A.CABA, Argentina</a></p>
                 </div>
             </div>
             <p className={`copy_footer ${styles.copy_footer}`}>Todos los derechos reservados <strong>&copy;</strong>{new Date().getFullYear()}</p>
